7e4be92b07f6ae267bffd8833264a77aae6a6429
gybalint3 <gyovaibalint6@gmail.com>
2026-09-07 08:49:24 +0200

    Add project files.

---
+++ GYB-gokartprojekt2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYB_gokartprojekt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*GYB 
+             * 2026-09-05
+             * Gokart időpontfoglaló - Egyénikis projekt
+             */
+
+            Console.WriteLine("GYB \n 2026-09-05 \n Gokart időpontfoglaló - Egyénikis projekt");
+        }
+    }
+}
