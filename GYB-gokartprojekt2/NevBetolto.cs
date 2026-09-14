4c27c885a894d3e3fca7b82b2249a38d41ca5bb3
gybalint3 <gyovaibalint6@gmail.com>
2026-09-14 11:40:05 +0200

    2rész

---
+++ GYB-gokartprojekt2/NevBetolto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GYB_gokartprojekt2
+{
+    internal class NevBetolto
+    {
+        public static List<string> NevekBetoltese(string fajlNev)
+        {
+            string tartalom = File.ReadAllText(fajlNev);
+
+           
+            tartalom = tartalom.Replace("'", "");
+
+            
+            string[] nevekTomb = tartalom.Split(',');
+
+            List<string> nevek = new List<string>();
+            foreach (string nev in nevekTomb)
+            {
+                nevek.Add(nev.Trim());
+            }
+
+            return nevek;
+        }
+    }
+}
---
+++ GYB-gokartprojekt2/Versenyzo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYB_gokartprojekt2
+{
+    internal class Versenyzo
+    {
+    }
+}
